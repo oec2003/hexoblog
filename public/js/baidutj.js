8bae1177f7fadf40e5946bf8eb4ba3a113109b15
oec2003 <oec2003@gmail.com>
2016-09-15 19:50:04 +0800

    add baidutj

---
+++ public/js/baidutj.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?53d9ef0df7e0c5f369cadd70fcfe5a08";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
